99c00031df71509f8f64471e2b0a7bde24cf5710
KaterinaStavrova <katerina.stavrova@stepadev.com>
2024-08-06 14:21:17 +0100

    some fixes IconSearchService

--- IconFinder/BusinessLogic/SearchService/IconSearchService.swift
+++ IconFinder/BusinessLogic/SearchService/IconSearchService.swift
@@ -15,146 +15,77 @@ class IconSearchService {
     private let resultsPerPage = 10
     
     func searchIcons(query: String, completion: @escaping ([IconModel]?, Error?) -> Void) {
-            guard var urlComponents = URLComponents(string: baseURL) else {
-                completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
-                return
-            }
-            
-            let queryItems: [URLQueryItem] = [
-                URLQueryItem(name: "query", value: query),
-                URLQueryItem(name: "count", value: "\(resultsPerPage)"),
-                URLQueryItem(name: "type", value: "png"),
-                URLQueryItem(name: "premium", value: "false"),
-                URLQueryItem(name: "offset", value: "\(currentPage * resultsPerPage)")
-            ]
-            urlComponents.queryItems = queryItems
-            
-            guard let url = urlComponents.url else {
-                completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
-                return
-            }
-            
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            request.timeoutInterval = 10
-            request.allHTTPHeaderFields = [
-                "Accept": "application/json",
-                "Authorization": "Bearer \(apiKey)"
-            ]
-            
-            //Try to load from cache
-            if let cachedResponse = URLCache.shared.cachedResponse(for: request) {
-                do {
-                    let decoder = JSONDecoder()
-                    let response = try decoder.decode(IconResponse.self, from: cachedResponse.data)
-                    let icons = response.icons
-                    completion(icons, nil)
-                } catch {
-                    completion(nil, error)
-                }
-                return
-            }
-            
-            //perform network request
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    completion(nil, error)
-                    return
-                }
-                
-                guard let data = data, let urlResponse = response else {
-                    completion(nil, NSError(domain: "No data or response", code: 0, userInfo: nil))
-                    return
-                }
-                
-                do {
-                    let decoder = JSONDecoder()
-                    let response = try decoder.decode(IconResponse.self, from: data)
-                    let icons = response.icons
-                    
-                    // Cache the response
-                    let cachedResponse = CachedURLResponse(response: urlResponse, data: data)
-                    URLCache.shared.storeCachedResponse(cachedResponse, for: request)
-                    
-                    completion(icons, nil)
-                } catch {
-                    completion(nil, error)
-                }
-            }
-            task.resume()
+        guard var urlComponents = URLComponents(string: baseURL) else {
+            completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
+            return
         }
         
-        func loadNextPage(query: String, completion: @escaping ([IconModel]?, Error?) -> Void) {
-            currentPage += 1
-            searchIcons(query: query, completion: completion)
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "count", value: "\(resultsPerPage)"),
+            URLQueryItem(name: "type", value: "png"),
+            URLQueryItem(name: "premium", value: "false"),
+            URLQueryItem(name: "offset", value: "\(currentPage * resultsPerPage)")
+        ]
+        urlComponents.queryItems = queryItems
+        
+        guard let url = urlComponents.url else {
+            completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
+            return
         }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+            "Accept": "application/json",
+            "Authorization": "Bearer \(apiKey)"
+        ]
+        
+        //Try to load from cache
+        if let cachedResponse = URLCache.shared.cachedResponse(for: request) {
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(IconResponse.self, from: cachedResponse.data)
+                let icons = response.icons
+                completion(icons, nil)
+            } catch {
+                completion(nil, error)
+            }
+            return
+        }
+        
+        //perform network request
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data, let urlResponse = response else {
+                completion(nil, NSError(domain: "No data or response", code: 0, userInfo: nil))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(IconResponse.self, from: data)
+                let icons = response.icons
+                
+                // Cache the response
+                let cachedResponse = CachedURLResponse(response: urlResponse, data: data)
+                URLCache.shared.storeCachedResponse(cachedResponse, for: request)
+                
+                completion(icons, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
     
-//    func searchIcons(query: String, completion: @escaping ([IconModel]?, Error?) -> Void) {
-//        guard var urlComponents = URLComponents(string: baseURL) else {
-//            completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
-//            return
-//        }
-//        
-//        let queryItems: [URLQueryItem] = [
-//            URLQueryItem(name: "query", value: query),
-//            URLQueryItem(name: "count", value: "10"),
-//            URLQueryItem(name: "type", value: "png"),
-//            URLQueryItem(name: "premium", value: "false")
-//        ]
-//        urlComponents.queryItems = queryItems
-//        
-//        guard let url = urlComponents.url else {
-//            completion(nil, NSError(domain: "Invalid URL", code: 0, userInfo: nil))
-//            return
-//        }
-//        
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//        request.timeoutInterval = 10
-//        request.allHTTPHeaderFields = [
-//            "Accept": "application/json",
-//            "Authorization": "Bearer \(apiKey)"
-//        ]
-//        
-//        // Try to load from cache first
-//        if let cachedResponse = URLCache.shared.cachedResponse(for: request) {
-//            do {
-//                let decoder = JSONDecoder()
-//                let response = try decoder.decode(IconResponse.self, from: cachedResponse.data)
-//                let icons = response.icons
-//                completion(icons, nil)
-//            } catch {
-//                completion(nil, error)
-//            }
-//            return
-//        }
-//        
-//        // Otherwise, perform network request
-//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-//            if let error = error {
-//                completion(nil, error)
-//                return
-//            }
-//            
-//            guard let data = data, let urlResponse = response else {
-//                completion(nil, NSError(domain: "No data or response", code: 0, userInfo: nil))
-//                return
-//            }
-//            
-//            do {
-//                let decoder = JSONDecoder()
-//                let response = try decoder.decode(IconResponse.self, from: data)
-//                let icons = response.icons
-//                
-//                // Cache the response
-//                let cachedResponse = CachedURLResponse(response: urlResponse, data: data)
-//                URLCache.shared.storeCachedResponse(cachedResponse, for: request)
-//                
-//                completion(icons, nil)
-//            } catch {
-//                completion(nil, error)
-//            }
-//        }
-//        task.resume()
-//    }
+    func loadNextPage(query: String, completion: @escaping ([IconModel]?, Error?) -> Void) {
+        currentPage += 1
+        searchIcons(query: query, completion: completion)
+    }
 }
